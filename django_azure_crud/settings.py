379dbd0236b71d973c7ae6588ba13231013287eb
Vitor Garcia de Almeida Lima <vitor.lima@programmers.com.br>
2022-10-30 17:23:02 -0300

    update 5

--- django_azure_crud/settings.py
+++ django_azure_crud/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8#cps-th^5$ydton0w+756io7qdm_*2dpmeag23j_ks99)_pi^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
